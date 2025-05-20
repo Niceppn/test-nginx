cddd3e5d301777ddd4c444b404542d8eb14f63ae
Niceppn <PPhutana01@gmail.com>
2025-05-20 10:23:40 +0700

    update host

--- accessibility/vite.config.js
+++ accessibility/vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', 
+    port: 3000,     
+  }
 })
+
 
 // import { defineConfig } from 'vite';
 // import path from 'path';
