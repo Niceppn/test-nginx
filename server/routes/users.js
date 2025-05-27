0a5796a284319c7c13fa25a23659b2fee47b546d
Niceppn <PPhutana01@gmail.com>
2025-05-27 12:01:15 +0700

    update user

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,16 @@
 var express = require('express');
 var router = express.Router();
+import mongoose from 'mongoose';
+import { connectionString } from '../config.js';
+
+mongoose.connect(connectionString)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
