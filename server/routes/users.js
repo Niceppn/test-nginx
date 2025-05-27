fab602b69e4e372b030cbe7c76a826732587b8fd
Niceppn <PPhutana01@gmail.com>
2025-05-27 12:12:14 +0700

    update

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var router = express.Router();
-import mongoose from 'mongoose';
-import { connectionString } from '../config.js';
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const { connectionString } = require('../config.js');
 
 mongoose.connect(connectionString)
   .then(() => {
@@ -10,7 +10,6 @@ mongoose.connect(connectionString)
   .catch((err) => {
     console.error("MongoDB connection error:", err);
   });
-
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
